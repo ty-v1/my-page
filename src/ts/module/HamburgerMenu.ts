b30b6a7c85e7751d0ee0c3aafc583a2cc5aae460
ty-v1 <y-tomida@ist.osaka-u.ac.jp>
2020-01-05 01:42:33 +0900

    ハンバーガーメニューを実装

---
+++ src/ts/module/HamburgerMenu.ts
@@ -0,0 +1,44 @@
+import {Module} from '@/ts/module/Module.ts';
+
+/**
+ * ハンバーガーメニューの制御を行う
+ * */
+export class HamburgerMenu implements Module {
+    private static readonly ID = 'st-hamburger-menu';
+    private static readonly MENU_ID = 'st-header__menu';
+    private static readonly ACTIVE = 'active';
+    private static readonly MENU_OPEN = 'open';
+
+    private readonly hamburger: HTMLElement;
+    private readonly menu: HTMLElement;
+    private isActive = false;
+
+    public constructor() {
+        this.hamburger = document.getElementById(HamburgerMenu.ID) as HTMLElement;
+        this.menu = document.getElementById(HamburgerMenu.MENU_ID) as HTMLElement;
+    }
+
+    public initialize(): void {
+        this.hamburger.addEventListener('click', () => this.onClick());
+    }
+
+    public onClick(): void {
+        if (!this.isActive) {
+            this.openMenu();
+        } else {
+            this.closeMenu();
+        }
+    }
+
+    private openMenu() {
+        this.isActive = true;
+        this.hamburger.classList.add(HamburgerMenu.ACTIVE);
+        this.menu.classList.add(HamburgerMenu.MENU_OPEN);
+    }
+
+    private closeMenu() {
+        this.isActive = false;
+        this.hamburger.classList.remove(HamburgerMenu.ACTIVE);
+        this.menu.classList.remove(HamburgerMenu.MENU_OPEN);
+    }
+}
