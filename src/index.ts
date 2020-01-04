8fb9e869a28ea71de1a3a93477485f2a87713117
ty-v1 <y-tomida@ist.osaka-u.ac.jp>
2020-01-05 01:45:46 +0900

    モジュールの初期化処理を追加

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
 import '@/scss/index.scss';
+import {HamburgerMenu} from '@/ts/module/HamburgerMenu.ts';
+import {Module} from '@/ts/module/Module.ts';
+
+const modules: Module[] = [];
+modules.push(new HamburgerMenu());
+
+modules.forEach(e => e.initialize());
